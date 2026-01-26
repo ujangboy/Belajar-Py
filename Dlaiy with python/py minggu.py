3965aeb687cf4879e35637ddd803989ec789ba67
UjangPrime <sankarulepus@gmail.com>
2026-01-26 10:58:00 +0700

    heloo world

--- Dlaiy with python/py minggu.py
+++ Dlaiy with python/py minggu.py
@@ -58,56 +58,11 @@ validate_username(username1)  # Output: True
 validate_username(username2)  # Output: False
 validate_username(username3)  # Output: False   
 
-#Join adalah metode untuk menggabungkan elemen-elemen dalam sebuah list/array menjadi satu string dengan pemisah tertentu.
-#Split adalah metode untuk memecah sebuah string menjadi list/array berdasarkan pemisah tertentu.
-
-hobi = ["membaca", "menulis", "berkebun"]
-hasil = ", ".join(hobi)
-print(hasil)  
-print(" ")
-
-data = "apel, maangga, pisang, jeruk "
-hasil = data.split(", ")
-print(hasil)
-
-print(" ")
-#soal latihan join dan split
-print("soal latihan join dan split")
-motto = "Berusaha-Berdoa-Bertawakal"
-kata_kata = motto.split("-")
-print(kata_kata)
 
 
-#pisahkan username dan domain menggunakan split
-#Pisahkan domain name dan ekstensi
-#Gabungkan kembali dengan format: "username - domain - ekstensi"
-
-email = "user@example.com"
-# Langkah 1
-bagian = email.split("@")  # ["user", "example.com"]
-username = bagian[0]
-domain_lengkap = bagian[1]
 
 
-# Langkah 2
-domain_bagian = domain_lengkap.split(".")  # ["example", "com"]
-domain_name = domain_bagian[0]
-ekstensi = domain_bagian[1]
 
 
-# Langkah 3
-hasil = " - ".join([username, domain_name, ekstensi])
-print(hasil)  # "user - example - com"
-
-#Buat program untuk mengubah format tanggal dari "02-11-2025" menjadi "02/11/2025"
-tanggalLama = "02-11-2025"
-
-bagian = tanggalLama.split("-")
-tanggalBaru = "/".join(bagian)
-print(tanggalBaru)  # Output: "02/11/2025"
-
-#Join hanya bisa menggabungkan list yang berisi STRING
-#Split tanpa parameter akan memisahkan berdasarkan whitespace apapun
-"a  b\tc\nd".split()  # ["a", "b", "c", "d"]
 
 
